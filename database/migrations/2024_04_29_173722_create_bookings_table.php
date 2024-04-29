02da71f7f18445ffb6ed83c9865af9e194399b90
alefthallysmartins <alef.thallys22@gmail.com>
2024-04-29 15:31:05 -0300

    Added a .txt file for database schema and endpoints, updated user migration, and added models/migrations for book, booking, and fine entities.

---
+++ database/migrations/2024_04_29_173722_create_bookings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('book_id')->constrained();
+          
+            $table->string('status')->default('pending');
+            $table->timestamp('borrowed_at')->nullable();
+            $table->timestamp('due_date')->nullable();
+            $table->timestamp('returned_at')->nullable();
+          
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
